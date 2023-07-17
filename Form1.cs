c62c55bc6e83de1d5321e76a302dc2a408259dad
FanyOliveira <135743057+FanyOliveira@users.noreply.github.com>
2023-07-17 17:32:37 -0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula06Streaming
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comédiaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Form2 telaInfo = new Form2();
+            telaInfo.Show();
+        }
+    }
+}
